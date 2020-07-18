240e656668c25f5459ee1838a60a8acb4a03ed4e
rainayangg <raina.yang.chn@gmail.com>
2020-07-18 17:42:15 +0800

    fix bug

--- tools/googlestd.cmd
+++ tools/googlestd.cmd
@@ -3,9 +3,9 @@ if [ ! -d /home/caida/outdir ]
 then
     mkdir /home/caida/outdir
 fi
-cp -r /home/caida/scratch/cloudspeedtest/outdir/* /home/caida/outdir/
+cp -r /home/caida/scratch/cloudspeedtest/outdir/ /home/caida/outdir/
 #restore possibly wrong active sibling
 mv /home/caida/outdir/datafiles/*.sibling.active /home/caida/outdir/datafiles/*.sibling.txt
-echo "google" >/home/caida/vmtype
+echo "googlestd" >/home/caida/vmtype
 mv /home/caida/outdir/datafiles/google.sibling.txt /home/caida/outdir/datafiles/google.sibling.active
 rm -rf /home/caida/scratch 
